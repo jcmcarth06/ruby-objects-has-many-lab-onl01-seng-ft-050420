091b8c3a729a0436622ee6816e125bd4eb49b178
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-19 17:53:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,7 +10,7 @@ class Artist #is initialized with a name
 
   def add_song(song) # takes in an argument of a song and associates that song with
                       # the artist by telling the song that it belongs to that artist
-    @songs << song
+    self.songs << song
     song.artist = self
     @@song_count += 1
   end
