507f32dcacc24c590c4c068d2cc40d846e92c330
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-19 18:07:20 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,12 +1,10 @@
+class Post  #is initialized with an argument of a title
+  attr_accessor :title, :author   #has a title
+                                  #belongs to an author
+    @@all = []
 
-
-class Post
-  attr_accessor :name, :artist
-
-  @@all = []
-
-  def initialize(name)
-    @name = name
+  def initialize(title)
+    @title = title
     @@all << self
   end
 
@@ -15,10 +13,10 @@ class Post
   end
 
   def author_name
-    if @author == nil
+    if @author == nil  #  returns nil if the post does not have an author
       return nil
     else
-      @author.name
+      @author.name   #knows the name of its author
     end
   end
 end
