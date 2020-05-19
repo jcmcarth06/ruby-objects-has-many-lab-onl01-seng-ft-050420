5bee6ceddd82e6a5889223939325affbd95e5d82
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-19 18:06:50 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -20,3 +20,26 @@ class Post  #is initialized with an argument of a title
     end
   end
 end
+
+class Post
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    if @author == nil
+      return nil
+    else
+      @author.name
+    end
+  end
+end
