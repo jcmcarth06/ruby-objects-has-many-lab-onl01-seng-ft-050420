b7d46258f0013c2e9e580f2f870268b252623178
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-19 17:59:53 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,6 +24,10 @@ class Artist #is initialized with a name
     @@song_count += 1
   end
 
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
   def self.song_count
     Song.all.count
   end
